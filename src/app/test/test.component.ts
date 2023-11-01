7b3c63384c733325fda4c70b446bd8e0278f8d3c
viktoriiaserhieieva <viktoriia.serhieieva@mynd.co>
2023-11-01 18:33:07 +0200

    Init app

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'test',
+  templateUrl: './test.component.html',
+  styleUrls: ['./test.component.css']
+})
+export class TestComponent {
+  title = 'myApp';
+}
